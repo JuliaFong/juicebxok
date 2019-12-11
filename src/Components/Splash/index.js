c15db89e429275a2d25d0cf0dd66036b0ee7317e
Julia Fong <juliafong@Julias-MacBook-Pro.local>
2019-12-10 16:21:35 -0800

    adds splash page image

--- src/Components/Splash/index.js
+++ src/Components/Splash/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import juice from './juicebx.jpg'
 import './splash.css' 
 
 const Splash = () => {
     return (
         <div className="splash">
             <h1>JuiceBx</h1>
+            <img src={juice}></img>
         </div>
     )
 }
